b73eb1ab4aa96d66104ecc450445c1c9b5de4680
DI <cdi122@nate.com>
2022-08-23 15:46:48 +0900

    웹 상의 이미지 불러오기(Glide 활용) - 인터넷 권한 획득 체험

--- app/src/main/java/com/example/librarypractice_di/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_di/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.librarypractice_di
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +16,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+        Glide.with(this).load("https://cloudfront-ap-northeast-1.images.arcpublishing.com/chosunbiz/4VTMIGOICP2DUYW6ZUWVKEDET4.png").into(lectureImg1)
+
     }
 }
