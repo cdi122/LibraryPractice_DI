9c3a609ae930da93b8c626f9d1619c25b57455c2
DI <cdi122@nate.com>
2022-09-01 13:56:45 +0900

    권한 라이브러리 활용 예시 화면 작성

--- app/src/main/java/com/example/librarypractice_di/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_di/MainActivity.kt
@@ -18,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         Glide.with(this).load("http://cloudfront-ap-northeast-1.images.arcpublishing.com/chosunbiz/4VTMIGOICP2DUYW6ZUWVKEDET4.png").into(lectureImg1)
+//  전화걸기버튼 누르면 권한확인 / 전화 연결
+        callBtn.setOnClickListener {
+//            라이브러리 활용 -> 전화 권한 확인 -> 실제 전화 연결
+        }
 
     }
 }
